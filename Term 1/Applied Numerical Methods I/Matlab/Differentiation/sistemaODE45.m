fea3fef11e94d9f2f31d249216daaa9f3afaba3e
Javier <blajavier@gmail.com>
2024-03-09 17:02:22 +0100

    Term 1 finished

--- Term 1/Applied Numerical Methods I/Matlab/Differentiation/sistemaODE45.m
+++
@@ -1,4 +0,0 @@
-function dydt = sistemaODE45(t, y)
-    % Establece el sistema de ecuaciones diferenciales de primer orden para pasar por "ode45"...
-    dydt = [y(2); y(1)*y(4)^2 - 3.986*10^14 / y(1)^2; y(4); -2*y(2)*y(4) / y(1)];
-end
